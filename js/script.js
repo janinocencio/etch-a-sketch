cb50aceec8eb38149e3751f7c70e5978a6b48e1e
janinocencio <janinocencio@gmail.com>
2023-12-21 16:12:24 +1100

    Create clear function to clear the grid and add square button functionality

--- js/script.js
+++ js/script.js
@@ -1,11 +1,38 @@
-for (let i=0; i<256; i++) { //i must be less than the number of squares
+function initialize (squaresRow) {
+    for (let i=0; i<squaresRow*squaresRow; i++) { //i must be less than the number of squares
+        const gridContainer = document.querySelector('.grid-container');
+        const oneSquare = document.createElement('div');
+        const oneSquareWidth = (gridContainer.getBoundingClientRect().width / squaresRow);
+        const oneSquareHeight = (gridContainer.getBoundingClientRect().height / squaresRow);
+        oneSquare.style.width = oneSquareWidth +"px";
+        oneSquare.style.height = oneSquareHeight +"px";
+        oneSquare.style.border = '1px dotted orange';
+        gridContainer.appendChild(oneSquare);
+    };    
+}
+
+function clear() {
     const gridContainer = document.querySelector('.grid-container');
-    const oneSquare = document.createElement('div');
-    const oneSquareWidth = (gridContainer.getBoundingClientRect().width / 16);
-    const oneSquareHeight = (gridContainer.getBoundingClientRect().height / 16);
-    oneSquare.style.width = oneSquareWidth +"px";
-    oneSquare.style.height = oneSquareHeight +"px";
-    oneSquare.style.border = 'orange dotted 1px';
-    gridContainer.appendChild(oneSquare);
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    };
 };
 
+
+
+const SQUARESDEFAULT = 16
+document.addEventListener('DOMContentLoaded', () => {
+    initialize(SQUARESDEFAULT);
+});
+
+const squareButton = document.querySelector('.square-number-button');
+squareButton.addEventListener('click', () => {
+    let squaresRow;
+    do {
+    squaresRow  = prompt("Enter desired number of squares per row/column: ");
+   } while (+squaresRow > 100 || Number.isNaN(+squaresRow) || squaresRow === "" || squaresRow.includes("."));
+   clear(); 
+   initialize(squaresRow);
+});
+
+const clearButton = document.querySelector('.clear-button')
